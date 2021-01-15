0ba8635f855380133752fd4395b8d71df3e39f5c
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-01-15 22:35:03 +0000

    checking whether spark works as expected

--- src/main/scala/playground/Playground.scala
+++ src/main/scala/playground/Playground.scala
@@ -1,7 +1,19 @@
 package playground
 
-object Playground extends App {
+import org.apache.spark.sql.SparkSession
 
-  println("Hello, world")
+object Playground1  {
+
+  val spark = SparkSession.builder()
+    .appName("Spark Optimization Playground")
+    .master("local")
+    .getOrCreate()
+
+  val sc = spark.sparkContext
+
+  def main(args: Array[String]): Unit = {
+    val rdd = sc.parallelize(1 to 1000)
+    println(s"I have my first RDD, it has ${rdd.count} rows. Now let me go optimize massive jobs.")
+  }
 
 }
